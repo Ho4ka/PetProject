a25f4302d1993a23c08c8cb2ca5e012998d8a99d
maksym.domaretskyi <m.domaretsky@gmail.com>
2019-05-26 18:25:54 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Hello erWorld!</h1>
+    </div>
+</template>
+<script>
+    export default {
+        name: "App"
+    }
+</script>
+
+<style lang="scss" >
+div {
+    h1 {
+        color: red;
+        }
+    }
+</style>
